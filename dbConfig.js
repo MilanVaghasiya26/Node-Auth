4eb29aaf3ee2ae99c938b9dcdd245f4179e12870
MilanVaghasiya <milanvaghasiya1126@gmail.com>
2022-10-17 15:03:03 +0530

    Add .gitignore files and env configure

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
 
+require("dotenv").config(); 
+
 const pool = new Pool( {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'nodelogin',
-    password: 'admin',
-    port: 5432,
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.port,
 });
 
 pool.connect(function (err) {
